88e0f40d0154c701d6da6562d5005a189cb32b68
ranyigit <ranyi_num@163.com>
2020-05-25 11:09:42 +0800

    路由守卫

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
